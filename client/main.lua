9a5d5ef2ca2026c702ee959dfaa18b8c8b39f34c
Marshular's Development <78507312+marshular@users.noreply.github.com>
2023-12-30 18:31:58 -0800

    code refactor
    - added picture command
    - merged the record command
    - remove rockstar editor button due to crashing
    - added register keymapping
    - added chat suggestions

--- client/main.lua
+++ client/main.lua
@@ -1,16 +1,24 @@
-RegisterCommand('recordstart', function()
-	StartRecording(1)
-end, false)
+TriggerEvent('chat:addSuggestion', '/record', 'Recording options', {
+    { name = "type", help = "start/stop/discard" }
+})
 
-RegisterCommand('recordstop', function()
-	StopRecordingAndSaveClip()
-end, false)
+TriggerEvent('chat:addSuggestion', '/picture', 'Take a picture')
 
-RegisterCommand('recorddiscard', function()
-	StopRecordingAndDiscardClip()
-end, false)
+RegisterCommand('record', function(args)
+	local type = args[1]
+	if type == 'start' then StartRecording(1)
+	if type == 'stop' then StopRecordingAndSaveClip()
+	if type == 'discard' then StopRecordingAndDiscardClip()
+	else return end
+end)
 
-RegisterCommand('rockstareditor', function()
-	NetworkSessionLeaveSinglePlayer()
-	ActivateRockstarEditor()
-end, false)
+RegisterCommand('picture', function()
+	BeginTakeHighQualityPhoto();
+	SaveHighQualityPhoto(-1);
+	FreeMemoryForHighQualityPhoto();
+end)
+
+RegisterKeyMapping('record start', '(Rockstar) Start Recording', 'keyboard', '')
+RegisterKeyMapping('record stop', '(Rockstar) Stop Recording', 'keyboard', '')
+RegisterKeyMapping('record discard', '(Rockstar) Discard Recording', 'keyboard', '')
+RegisterKeyMapping('picture', '(Rockstar) Take a Picture', 'keyboard', '')
